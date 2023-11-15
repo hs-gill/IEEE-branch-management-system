58d2803cfe948418bdc64038fa6c380e05a13eb7
mackenzieflewelling <maf653@usask.ca>
2023-11-15 13:11:24 -0600

    Updated the privacy policy texts.

--- resources/js/Pages/PrivacyPolicy/Main.vue
+++ resources/js/Pages/PrivacyPolicy/Main.vue
@@ -27,33 +27,42 @@ import AppLayout from "@/Layouts/AppLayout.vue";
             <div class="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
                 <div class="lg:pr-4">
                     <div class="max-w-xl text-base leading-7 text-gray-700 dark:text-gray-400 lg:max-w-lg">
-                        <p>We ensure that all personal information is stored securely and is not shared with third parties without explicit consent. Our members' data is used exclusively for purposes related to the branch's activities, such as organizing social events,
-                            academic support services, conferences, and industry tours, as well as for community outreach initiatives.</p>
+                        <p>The University of Saskatchewan is committed to protecting the privacy and confidentiality of personal information. All personal information that is collected by the University of Saskatchewan, whether through this website (www.usask.ca) or by other means, is managed according to the Saskatchewan Local Authority Freedom of Information and
+                            Protection of Privacy Act and the University’s Data Management policy.</p>
                         <ul role="list" class="mt-8 space-y-8 text-gray-600 dark:text-gray-400">
                             <li class="flex gap-x-3">
                                 <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                     <path fill-rule="evenodd" d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z" clip-rule="evenodd" />
                                 </svg>
-                                <span><strong class="font-semibold text-gray-900 dark:text-gray-200">Right to Know.</strong> Our privacy policy ensures every member has the right to be informed about the data we collect and how it is managed, maintaining transparency and trust. </span>
+                                <span><strong class="font-semibold text-gray-900 dark:text-gray-200">Automatic Collection, Use and Disclosure of Information</strong> Information collected automatically is maintained in a secure environment. Summary reports will be used for purposes such as assessing system performance, improving web services and website management.
+                                    The University will not use this data for other purposes, unless required or permitted by law. </span>
                             </li>
                             <li class="flex gap-x-3">
                                 <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                     <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
                                 </svg>
-                                <span><strong class="font-semibold text-gray-900 dark:text-gray-200">Collection and Usage of Data.</strong> We are committed to responsibly collecting and utilizing personal data solely for enhancing our branch activities and member experiences, as outlined in our privacy policy.</span>
+                                <span><strong class="font-semibold text-gray-900 dark:text-gray-200">Information Provided Voluntarily in E-mail and Web Forms</strong> Personal information provided voluntarily in an e-mail or by filling out a form will be used only for the purposes for which it is collected. This information may also be shared with our customer service representatives, Webmaster,
+                                    Web Administrator or other employees as a means to continually improve customer service.</span>
                             </li>
                             <li class="flex gap-x-3">
                                 <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                     <path d="M4.632 3.533A2 2 0 016.577 2h6.846a2 2 0 011.945 1.533l1.976 8.234A3.489 3.489 0 0016 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234z" />
                                     <path fill-rule="evenodd" d="M4 13a2 2 0 100 4h12a2 2 0 100-4H4zm11.24 2a.75.75 0 01.75-.75H16a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75h-.01a.75.75 0 01-.75-.75V15zm-2.25-.75a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 00.75-.75V15a.75.75 0 00-.75-.75h-.01z" clip-rule="evenodd" />
                                 </svg>
-                                <span><strong class="font-semibold text-gray-900 dark:text-gray-200">Handling of Personal Information.</strong> The handling of personal information is governed by strict confidentiality and security measures, as per our privacy policy, to protect member data integrity and privacy.</span>
+                                <span><strong class="font-semibold text-gray-900 dark:text-gray-200">Links to Third Party Sites</strong> University of Saskatchewan websites contain links to other sites. While the University of Saskatchewan endeavours to link to other sites that have similar high standards and respect for privacy, we are not responsible for the privacy practices or the content of other sites.</span>
                             </li>
                         </ul>
                         <p class="mt-8">This policy is subject to updates,
                             and members will be notified of any significant changes.</p>
-                        <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-200">Need help? </h2>
-                        <p class="mt-6">For more detailed information or any queries regarding our privacy practices, please feel free to reach out to us through the contact page on our website. Our dedicated team at the IEEE Usask Student Branch is always ready to provide clarifications and address any concerns you might have about your data and privacy. Your trust and confidence in our policies are paramount to us, and we are committed to ensuring your experience with our branch is both positive and secure.</p>
+                        <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-200">More information </h2>
+                        <p class="more-information">
+                           <br> More information<br>
+                            Access and Privacy Officer<br>
+                            Legal Office<br>
+                            PMB 136 - 107 Administration Place<br>
+                            Saskatoon, SK S7N 5A2<br>
+                            E-mail: <a href="mailto:privacy@usask.ca">privacy@usask.ca</a>
+                        </p>
                     </div>
                 </div>
             </div>
