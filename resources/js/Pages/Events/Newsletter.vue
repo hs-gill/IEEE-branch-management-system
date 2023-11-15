38d1a34e705aa0c1dc107a7d679c47d7ca35a1b9
mackenzieflewelling <maf653@usask.ca>
2023-11-15 12:49:56 -0600

    Updated the privacy policy texts.
    Reverted events page.

--- resources/js/Pages/Events/Newsletter.vue
+++ resources/js/Pages/Events/Newsletter.vue
@@ -9,10 +9,10 @@
             <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
                 <div class="max-w-xl lg:max-w-lg">
                     <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Subscribe to our newsletter.</h2>
-                    <p class="mt-4 text-lg leading-8 text-gray-300">The IEEE is proud to promote their weekly activities and newsletters.</p>
+                    <p class="mt-4 text-lg leading-8 text-gray-300">Nostrud amet eu ullamco nisi aute in ad minim nostrud adipisicing velit quis. Duis tempor incididunt dolore.</p>
                     <div class="mt-6 flex max-w-md gap-x-4">
                         <label for="email-address" class="sr-only">Email address</label>
-<!--                        <input id="email-address" name="email" type="email" autocomplete="email" required class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" placeholder="Enter your email">-->
+                        <!--                        <input id="email-address" name="email" type="email" autocomplete="email" required class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" placeholder="Enter your email">-->
                         <button type="submit" class="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Subscribe</button>
                     </div>
                 </div>
@@ -24,7 +24,7 @@
                             </svg>
                         </div>
                         <dt class="mt-4 font-semibold text-white">Weekly articles</dt>
-                        <dd class="mt-2 leading-7 text-gray-400">A calender for weekly articles.</dd>
+                        <dd class="mt-2 leading-7 text-gray-400">Non laboris consequat cupidatat laborum magna. Eiusmod non irure cupidatat duis commodo amet.</dd>
                     </div>
                     <div class="flex flex-col items-start">
                         <div class="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
@@ -33,7 +33,7 @@
                             </svg>
                         </div>
                         <dt class="mt-4 font-semibold text-white">No spam</dt>
-                        <dd class="mt-2 leading-7 text-gray-400">Insert No Spam Thing Here.</dd>
+                        <dd class="mt-2 leading-7 text-gray-400">Officia excepteur ullamco ut sint duis proident non adipisicing. Voluptate incididunt anim.</dd>
                     </div>
                 </dl>
             </div>
