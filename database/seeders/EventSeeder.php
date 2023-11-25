350d66d94dc6ca89dd8f5ed76678c63c539408cd
Rodrigo Godoy <rjgodoy@icloud.com>
2023-11-25 11:33:30 -0600

    Improvement to Events page

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,35 @@ class EventSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Event::factory(6)->create();
+        $events = [
+            [
+                'title' => 'Pizza Sale',
+                'description' => 'title',
+                'image_path' => '/images/events/Pizza_Sale_PDF.jpg',
+                'dated_at' => new Carbon('first day of January 2024', 'America/Vancouver')
+            ],
+            [
+                'title' => 'IEEE Bxxr Night',
+                'description' => 'title',
+                'image_path' => '/images/events/IEEE_Bxxr_Night_Nov_17th-1.jpg',
+                'dated_at' => new Carbon('first day of November 2023', 'America/Vancouver')
+            ],
+            [
+                'title' => 'Movember',
+                'description' => 'title',
+                'image_path' => '/images/events/Movember_Poster.jpg',
+                'dated_at' => new Carbon('first day of November 2023', 'America/Vancouver')
+            ],
+        ];
+
+        foreach($events as $event) {
+            \App\Models\Event::create([
+                'title' => $event['title'],
+                'description' => $event['description'],
+                'image_path' => $event['image_path'],
+                'dated_at' => $event['dated_at'],
+            ]);
+        }
+
     }
 }
--- resources/js/Pages/Events/Show.vue
+++ resources/js/Pages/Events/Show.vue
@@ -2,12 +2,14 @@
 import AppLayout from '@/Layouts/AppLayout.vue';
 import Newsletter from "@/Pages/Events/Newsletter.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
-import {router, useForm} from "@inertiajs/vue3";
+import {Link, router, useForm} from "@inertiajs/vue3";
 import {onMounted} from "vue";
 
 const props = defineProps({
     event: Object,
 });
+
+const event = props.event.data;
 
 onMounted(() => {
     console.log(props.event)
@@ -30,32 +32,17 @@ const submit = () => {
     <AppLayout title="Events">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                Event {{ event.data.title }}
+                Event {{ event.title }}
             </h2>
         </template>
 
         <div class="max-w-7xl mx-auto p-6 lg:p-8">
             <div class="mt-4">
-                <div class="">
-                    <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex focus:outline focus:outline-2 focus:outline-red-500">
-                        <div>
-                            <div class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-7 h-7 stroke-red-500">
-                                    <path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
-                                </svg>
-                            </div>
-                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">{{ event.data.title }}</h2>
-                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                {{ event.data.description }}
-                            </p>
-                        </div>
-                    </div>
-                </div>
+                <img :src="event.image_path" alt="" class="rounded-lg">
             </div>
 
-
             <div class="mt-4">
-                <div class="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-lg sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+                <div class="relative isolate overflow-hidden bg-white dark:bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-lg sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
                     <svg viewBox="0 0 1024 1024" class="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0" aria-hidden="true">
                         <circle cx="512" cy="512" r="512" fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fill-opacity="0.7" />
                         <defs>
@@ -66,24 +53,21 @@ const submit = () => {
                         </defs>
                     </svg>
                     <div class="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
-                        <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Do you want to volunteer?<br>Be part of our team!</h2>
-                        <p class="mt-6 text-lg leading-8 text-gray-300">Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.</p>
+                        <h2 class="text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">Do you want to volunteer?<br>Be part of our team!</h2>
+                        <p class="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300">Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.</p>
                         <div class="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
                             <form @submit.prevent="submit">
                                 <PrimaryButton class="rounded-md px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                                     Be a volunteer
                                 </PrimaryButton>
                             </form>
-
                         </div>
                     </div>
                     <div class="relative mt-16 h-80 lg:mt-8">
-                        <img class="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10" src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png" alt="App screenshot" width="1824" height="1080">
+                        <img class="absolute left-0 top-0 w-[57rem] max-w-none rounded-md opacity-50" src="/images/volunteer.png" alt="App screenshot">
                     </div>
                 </div>
             </div>
-
-
         </div>
 
         <Newsletter/>
