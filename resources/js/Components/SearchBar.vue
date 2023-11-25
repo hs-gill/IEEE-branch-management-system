a42edc0471f7720ab76e151d5782e9d37bae1543
Rodrigo Godoy <rjgodoy@icloud.com>
2023-11-25 16:36:48 -0600

    improvements in textbooks and items

--- resources/js/Components/SearchBar.vue
+++ resources/js/Components/SearchBar.vue
@@ -21,7 +21,7 @@ defineExpose({ focus: () => input.value.focus() });
 
 <template>
 
-    <div class="max-w-3xl mx-auto dark:bg-gray-800 border-0">
+    <div class="max-w-3xl mx-auto dark:bg-transparent border-0">
         <div class="relative flex items-center w-full h-12 rounded-lg bg-white dark:bg-gray-900">
             <div class="grid place-items-center h-full w-12 text-gray-300">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
--- resources/js/Pages/Textbooks/Main.vue
+++ resources/js/Pages/Textbooks/Main.vue
@@ -29,37 +29,41 @@ function filteredList() {
         <div class="max-w-7xl mx-auto p-6 lg:p-8">
             <div class="">
 
-                <div class="bg-white dark:bg-gray-800">
-                    <div class="p-8">
-                        <SearchBar
-                            id="name"
-                            v-model="input"
-                            type="text"
-                            class="block w-full mt-1"
-                            placeholder="Search items..."
-                            autofocus
-                        />
-                    </div>
+                <div class="mt-5 lg:mt-7 md:grid-cols-2 gap-6 lg:gap-8">
+                    <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex focus:outline focus:outline-2 focus:outline-red-500">
+                        <div>
+                            <div class="p-2">
+                                <SearchBar
+                                    id="name"
+                                    v-model="input"
+                                    type="text"
+                                    class="block w-full mt-1"
+                                    placeholder="Search items..."
+                                    autofocus
+                                />
+                            </div>
 
-                    <div class="mx-auto max-w-2xl px-4 py-4 sm:px-6 sm:py-8 lg:max-w-7xl lg:px-8">
-                        <h2 class="text-4xl font-bold tracking-tight text-gray-900 dark:text-white">Textbooks</h2>
+<!--                            <div class="mx-auto max-w-2xl px-4 py-4 sm:px-6 sm:py-8 lg:max-w-7xl lg:px-8">-->
+<!--                                <h2 class="text-4xl font-bold tracking-tight text-gray-900 dark:text-white">Textbooks</h2>-->
 
-                        <div  class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                            <Link v-for="textbook in filteredList()" :key="textbook.id" :href="`textbooks/${textbook.id}`" class="group relative">
-                                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                                    <img :src="textbook.cover" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                                <div  class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                                    <Link v-for="textbook in filteredList()" :key="textbook.id" :href="`textbooks/${textbook.id}`" class="group relative">
+                                        <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                                            <img :src="textbook.cover" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                                        </div>
+                                        <div class="mt-4 flex justify-between">
+                                            <div>
+                                                <h3 class="text-sm text-gray-700 dark:text-gray-100">
+                                                    <span aria-hidden="true" class="absolute inset-0"></span>
+                                                    {{ textbook.title }}
+                                                </h3>
+                                                <p class="mt-1 text-sm text-gray-500 dark:text-gray-400">{{ textbook.author }}</p>
+                                            </div>
+                                            <p class="text-sm font-medium text-gray-900 dark:text-gray-200">$35</p>
+                                        </div>
+                                    </Link>
                                 </div>
-                                <div class="mt-4 flex justify-between">
-                                    <div>
-                                        <h3 class="text-sm text-gray-700 dark:text-gray-100">
-                                            <span aria-hidden="true" class="absolute inset-0"></span>
-                                            {{ textbook.title }}
-                                        </h3>
-                                        <p class="mt-1 text-sm text-gray-500 dark:text-gray-400">{{ textbook.author }}</p>
-                                    </div>
-                                    <p class="text-sm font-medium text-gray-900 dark:text-gray-200">$35</p>
-                                </div>
-                            </Link>
+<!--                            </div>-->
                         </div>
                     </div>
                 </div>
