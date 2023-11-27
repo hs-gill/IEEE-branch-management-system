a247e1a3ee9a4d63bad3f47999b75c723e521609
Rodrigo Godoy <rjgodoy@icloud.com>
2023-11-27 01:03:04 -0600

    Improvements and bug fix in textbooks

--- resources/js/Pages/Textbooks/Show.vue
+++ resources/js/Pages/Textbooks/Show.vue
@@ -4,16 +4,13 @@ import PrimaryButton from "@/Components/PrimaryButton.vue";
 import {Link, useForm} from "@inertiajs/vue3";
 import {onMounted, ref} from "vue";
 
-
 const props = defineProps({
     textbook: Object,
 });
 
-onMounted(() => {
-    console.log(props.textbook)
-})
-
-const price = ref(0.50)
+const open = ref(false);
+const openCart = () => open.value = true;
+const closeCart = () => open.value = false;
 
 const book = props.textbook.data
 
@@ -25,17 +22,13 @@ const addToCart = () => {
     form.put(route('cart.add-to-cart'), {
         errorBag: 'addToCart',
         preserveScroll: true,
-        onSuccess: () => true,
-        // onFinish: () => titleInput.value.focus(),
+        onSuccess: () => openCart(),
     });
 };
-
-
-
 </script>
 
 <template>
-    <AppLayout title="Items">
+    <AppLayout title="Items" :toOpen="open" @openCart="openCart" @closeCart="closeCart">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
                 Item {{ book.title }}
@@ -60,7 +53,7 @@ const addToCart = () => {
                     <!-- Options -->
                     <div class="mt-4 lg:row-span-3 lg:mt-0">
                         <h2 class="sr-only">Product information</h2>
-                        <p class="text-3xl tracking-tight text-gray-900 dark:text-gray-200">$ {{ price }}</p>
+                        <p class="text-3xl tracking-tight text-gray-900 dark:text-gray-200">$ {{ book.price }}</p>
 
                         <!-- Reviews -->
                         <!--                        <div class="mt-6">-->
@@ -125,7 +118,7 @@ const addToCart = () => {
                         </div>
                     </div>
 
-                    <div class="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 dark:lg:border-gray-800 lg:pb-16 lg:pr-8 lg:pt-6">
+                    <div class="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 dark:lg:border-gray-800 lg:pb-4 lg:pr-8 lg:pt-4">
                         <!-- Description and details -->
                         <div>
                             <h3 class="sr-only">Description</h3>
@@ -155,50 +148,19 @@ const addToCart = () => {
                         <!--                            </div>-->
                         <!--                        </div>-->
                     </div>
+
+                    <div v-if="props.textbook.can.update" class="items-center mt-4">
+                        <div class="flex justify-start">
+                            <Link :href="`${textbook.id}/edit` " class="">
+                                <PrimaryButton class="ml-4">
+                                    Edit Product
+                                </PrimaryButton>
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-
-
-
-
-
-
-
-        <div class="max-w-7xl mx-auto p-6 lg:p-8">
-            <!--            <div class="mt-4">-->
-            <!--                <div class="">-->
-            <!--                    <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">-->
-            <!--                        <div>-->
-            <!--                            <div class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">-->
-            <!--                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-7 h-7 stroke-red-500">-->
-            <!--                                    <path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />-->
-            <!--                                </svg>-->
-            <!--                            </div>-->
-            <!--                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">{{ item.title }}</h2>-->
-            <!--                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">-->
-            <!--                                {{ item.description }}-->
-            <!--                            </p>-->
-            <!--                        </div>-->
-            <!--                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">-->
-            <!--                            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />-->
-            <!--                        </svg>-->
-            <!--                    </div>-->
-            <!--                </div>-->
-            <!--            </div>-->
-
-            <div v-if="props.textbook.can.update" class="items-center mt-4">
-                <div class="flex justify-start">
-                    <Link :href="`${product.id}/edit` " class="">
-                        <PrimaryButton class="ml-4">
-                            Edit Product
-                        </PrimaryButton>
-                    </Link>
-                </div>
-            </div>
-
-        </div>
-
 
     </AppLayout>
 </template>
